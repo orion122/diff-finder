3ab0198860efd8151bd839841bf55eaab6d07772
ramazan <yaru@bk.ru>
2017-08-28 10:29:02 +0300

    add test for plain format

--- tests/diffFinderTest.php
+++ tests/diffFinderTest.php
@@ -3,11 +3,9 @@ namespace Diff\Tests;
 
 class DiffFinderTest extends \PHPUnit_Framework_TestCase
 {
-    public $format = 'pretty';
-
     public function testFindDiff()
     {
-        $findDiffResult = '{
+        $diffResult = '{
     "host": "hexlet.io"
   - "timeout": "50"
   + "timeout": "20"
@@ -17,28 +15,28 @@ class DiffFinderTest extends \PHPUnit_Framework_TestCase
         $array1 = ['host' => 'hexlet.io', 'timeout' => 50, 'proxy' => '123.234.53.22'];
         $array2 = ['timeout' => 20, 'verbose' => true, 'host' => 'hexlet.io'];
 
-        $this->assertEquals("$findDiffResult", \DiffFinder\diff\findDiff($array1, $array2, $this->format));
+        $this->assertEquals("$diffResult", \DiffFinder\diff\findDiff($array1, $array2, 'pretty'));
     }
 
-    public function testNestedFindDiff()
+    public function testFindDiffNestedPretty()
     {
         $array1 = ["common" => [
-                "setting1" => "Value 1",
-                "setting2" => "200",
-                "setting3" => true,
-                "setting6" => [
-                    "key" => "value"
-                ]
+            "setting1" => "Value 1",
+            "setting2" => "200",
+            "setting3" => true,
+            "setting6" => [
+                "key" => "value"
+            ]
         ],
             "group1" => [
                 "baz" => "bas",
                 "foo" => "bar"
-                ],
+            ],
             "group2" => [
                 "abc" => "12345"
             ]];
 
-        $array2 =["common" => [
+        $array2 = ["common" => [
             "setting1" => "Value 1",
             "setting3" => true,
             "setting4" => "blah blah",
@@ -54,7 +52,7 @@ class DiffFinderTest extends \PHPUnit_Framework_TestCase
                 "fee" => "100500"
             ]];
 
-        $findDiffResult = '{
+        $diffResult = '{
     "common": {
         "setting1": "Value 1"
       - "setting2": "200"
@@ -80,6 +78,52 @@ class DiffFinderTest extends \PHPUnit_Framework_TestCase
     }
 }';
 
-        $this->assertEquals("$findDiffResult", \DiffFinder\diff\findDiff($array1, $array2, $this->format));
+        $this->assertEquals("$diffResult", \DiffFinder\diff\findDiff($array1, $array2, 'pretty'));
+    }
+
+
+    public function testFindDiffNestedPlain()
+    {
+        $array1 = ["common" => [
+            "setting1" => "Value 1",
+            "setting2" => "200",
+            "setting3" => true,
+            "setting6" => [
+                "key" => "value"
+            ]
+        ],
+            "group1" => [
+                "baz" => "bas",
+                "foo" => "bar"
+            ],
+            "group2" => [
+                "abc" => "12345"
+            ]];
+
+        $array2 = ["common" => [
+            "setting1" => "Value 1",
+            "setting3" => true,
+            "setting4" => "blah blah",
+            "setting5" => [
+                "key5" => "value5"
+            ]
+        ],
+            "group1" => [
+                "foo" => "bar",
+                "baz" => "bars"
+            ],
+            "group3" => [
+                "fee" => "100500"
+            ]];
+
+        $diffResult = "Property 'common.setting2' was removed
+Property 'common.setting6' was removed
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: 'complex value'
+Property 'group1.baz' was changed. From 'bas' to 'bars'
+Property 'group2' was removed
+Property 'group3' was added with value: 'complex value'\n";
+
+        $this->assertEquals("$diffResult", \DiffFinder\diff\findDiff($array1, $array2, 'plain'));
     }
 }
