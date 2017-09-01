53f41c52298cfd4ac9a17cf5593f83f6cb525c9a
ramazan <yaru@bk.ru>
2017-09-01 18:34:16 +0300

    array_map and flatten instead array_reduce and concatenation in plain.php

--- src/output/plain.php
+++ src/output/plain.php
@@ -2,43 +2,39 @@
 
 namespace DiffFinder\output\plain;
 
-function genOutput($AST, $parents = '')
-{
-    $result = array_reduce($AST, function ($acc, $array) use ($parents) {
-        if ($array['type'] === 'nested') {
-            $acc .= genOutput($array['children'], "$parents{$array['key']}.");
-        } else {
-            if ($array['type'] === 'changed') {
-                $acc .= buildLine('changed', $parents . $array['key'], $array['from'], $array['to']);
-            } elseif ($array['type'] === 'removed') {
-                $acc .= buildLine('removed', $parents . $array['key']);
-            } elseif ($array['type'] === 'added') {
-                if (is_array($array['from'])) {
-                    $acc .= buildLine('added', $parents . $array['key'], 'complex value');
-                } else {
-                    $acc .= buildLine('added', $parents . $array['key'], $array['from']);
-                }
-            }
-        }
-
-        return $acc;
-    }, '');
-
-    return $result;
-}
-
+use function \Funct\Collection\flattenAll;
 
 function buildLine($changeType, $property, $value1 = '', $value2 = '')
 {
-    $line = '';
-
     if ($changeType === 'removed') {
-        $line = "'$property' was $changeType";
+        return "Property '$property' was $changeType";
     } elseif ($changeType === 'added') {
-        $line = "'$property' was $changeType with value: '$value1'";
+        return "Property '$property' was $changeType with value: '$value1'";
     } elseif ($changeType === 'changed') {
-        $line = "'$property' was $changeType. From '$value1' to '$value2'";
+        return "Property '$property' was $changeType. From '$value1' to '$value2'";
     }
+}
 
-    return "Property $line\n";
+
+function genOutput($AST, $parents = '')
+{
+    $result = array_map(function ($array) use ($parents) {
+        if ($array['type'] === 'nested') {
+            return genOutput($array['children'], "$parents{$array['key']}.");
+        } else {
+            if ($array['type'] === 'changed') {
+                return buildLine('changed', $parents . $array['key'], $array['from'], $array['to']);
+            } elseif ($array['type'] === 'removed') {
+                return buildLine('removed', $parents . $array['key']);
+            } elseif ($array['type'] === 'added') {
+                if (is_array($array['from'])) {
+                    return buildLine('added', $parents . $array['key'], 'complex value');
+                } else {
+                    return buildLine('added', $parents . $array['key'], $array['from']);
+                }
+            }
+        }
+    }, $AST);
+
+    return implode(PHP_EOL, array_filter(flattenAll($result)));
 }
