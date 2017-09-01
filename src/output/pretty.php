2040cfaabe58c4f95f7f64359d30ce6e1c8571c7
ramazan <yaru@bk.ru>
2017-09-01 16:25:01 +0300

    rename buildLine()

--- src/output/pretty.php
+++ src/output/pretty.php
@@ -10,17 +10,17 @@ function output($AST, $depth = 0)
     $result = array_reduce($AST, function ($acc, $array) use ($depth) {
         if ($array['type'] === 'nested') {
             $value = output($array['children'], $depth + 1);
-            $acc .= build(true, " ", $array['key'], $value, $depth);
+            $acc .= buildLine(true, " ", $array['key'], $value, $depth);
         } else {
             if ($array['type'] === 'unchanged') {
-                $acc .= build(false, " ", $array['key'], $array['from'], $depth);
+                $acc .= buildLine(false, " ", $array['key'], $array['from'], $depth);
             } elseif ($array['type'] === 'changed') {
-                $acc .= build(false, "-", $array['key'], $array['from'], $depth);
-                $acc .= build(false, "+", $array['key'], $array['to'], $depth);
+                $acc .= buildLine(false, "-", $array['key'], $array['from'], $depth);
+                $acc .= buildLine(false, "+", $array['key'], $array['to'], $depth);
             } elseif ($array['type'] === 'removed') {
-                $acc .= build(false, "-", $array['key'], $array['from'], $depth);
+                $acc .= buildLine(false, "-", $array['key'], $array['from'], $depth);
             } elseif ($array['type'] === 'added') {
-                $acc .= build(false, "+", $array['key'], $array['from'], $depth);
+                $acc .= buildLine(false, "+", $array['key'], $array['from'], $depth);
             }
         }
 
@@ -31,7 +31,7 @@ function output($AST, $depth = 0)
 }
 
 
-function build($isNested, $mark, $key, $value, $depth)
+function buildLine($isNested, $mark, $key, $value, $depth)
 {
     $spaces = str_repeat(' ', $depth * 4 + 2);
 
